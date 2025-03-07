419b569401b94f21e9b18357d42c401662054809
Rahul Solanki <rahul.solanki@growexx.com>
2025-03-07 12:31:47 +0530

    Show tasks using the dummy json API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from './task.service';
+import { FormsModule } from '@angular/forms';
+import { Task } from './task';
+import { TasksResponse } from './tasks-response';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [FormsModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title: string = 'my-app'
+export class AppComponent implements OnInit {
+
+  tasks: Task[] = [];
+  newTask: string = '';
+  isLoading: boolean = true;
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.taskService.getTasks().subscribe((data: TasksResponse) => {
+      this.tasks = data.todos;
+      this.isLoading = false;
+    });
+  }
+
+  onAddTask() {
+    this.tasks.unshift({id: this.tasks.length+1, todo: this.newTask, completed: false});
+    this.newTask = '';
+  }
 
 }
